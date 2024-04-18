597b87bf0b70c87907fdb41ac1e968ed894b7067
Volker Simon <volker@simongonzalez.eu>
2024-04-18 23:35:19 +0200

    fixes

--- game/main.gd
+++ game/main.gd
@@ -2,8 +2,8 @@ extends Node
 
 func _ready() -> void:
 	print("Hallo")
-	var s = FileInfo.new("c:\\vfcompat.dll")
-	#s.set_file(("c:\\vfcompat.dll"))
+	var s = FileInfo.new()
+	s.set_file(("c:\\vfcompat.dll"))
 	print(s.day())
 	print(s.get_filedate())
 	print(s.get_filedatetime())
